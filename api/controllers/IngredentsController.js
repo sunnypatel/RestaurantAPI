50642ea2c5303e3545754f64e07a552bcc5e285e
Sunny Patel <spatel@leadnomics.com>
2014-11-23 13:18:34 -0500

    Creates Ingredents

---
+++ api/controllers/IngredentsController.js
@@ -0,0 +1,11 @@
+/**
+ * IngredentsController
+ *
+ * @description :: Server-side logic for managing ingredents
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
