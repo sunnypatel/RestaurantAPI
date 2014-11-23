651637f67f52ff8fdd4e049bc8dc30671373ef80
Sunny Patel <spatel@leadnomics.com>
2014-11-23 12:44:40 -0500

    Creates Item api

---
+++ api/models/Item.js
@@ -0,0 +1,21 @@
+/**
+* Item.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+    attributes: {
+        name: {
+            type: 'STRING'
+        },
+        price: {
+            type: 'FLOAT'
+        },
+        restaurant: {
+            model: 'restaurant'
+        }
+    }
+};
