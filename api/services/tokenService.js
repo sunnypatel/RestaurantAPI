488c3e2e05dc7fbdc484b9da6f0f672da085eb97
Sunny Patel <spatel@leadnomics.com>
2014-11-23 12:15:17 -0500

    Created User api

--- api/services/tokenService.js
+++ api/services/tokenService.js
@@ -1,11 +1,39 @@
+var TAG = "TokenService: ";
+
 module.exports = {
-    isExpired: function (datetimeToChk) {
-        var now = new Date().getTime();
-        if (datetimeToChk > now) {
-            console.log("in the future");
+    /**
+     * Check if token is expired,
+     * @Params token object or token id
+     */
+    isExpired: function (token) {
+        if (token.expiresAt) {
+            console.log(TAG + "Got token object");
+            datetimeToChk = token.expiresAt;
+            var now = new Date().getTime();
+            if (datetimeToChk > now) {
+                console.log(TAG + "Token valid");
+                return false;
+            } else {
+                console.log(TAG + "Token expired");
+                return true;
+            }
+        } else {
+            console.log(TAG + "Searching by apiToken");
+            return Token.findOne({
+                apiToken: token
+            })
+            .exec(function (err, found){
+                if (found)
+                    return TokenService.isExpired(found);
+                else {
+                    console.log(TAG + "Unknown param...returning false");
+                    return false;
+                }
+            })
         }
     },
     generateToken: function() {
+        console.log(TAG + "Generating new token...");
         var uuid = require('node-uuid');
         var expireAfterMin = 60;
         var now = new Date().getTime();
@@ -18,15 +46,14 @@ module.exports = {
             hasExpired: false
         })
         .then(function (created){
-            console.log("Created new token: " + created.apiToken);
+            console.log(TAG + "Generated new token");
             return {
-                id: created.id,
                 apiToken: created.apiToken,
                 expiresAt: expiresAt
             };
         })
         .catch(function (err){
-            console.log('Error generateToken: ' + err);
+            console.log(TAG + 'Error generateToken: ' + err);
             return { err: err };
         });
     }
--- config/policies.js
+++ config/policies.js
@@ -28,7 +28,6 @@ module.exports.policies = {
 
   // '*': true,
   UserController: {
-    new: ['isLoggedIn','isAdmin'],
   }
   /***************************************************************************
   *                                                                          *
