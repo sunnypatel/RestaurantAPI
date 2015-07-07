c53ecf57d899800641a5928e1c772a7ad2c018bc
Sunny Patel <du.svp28@gmail.com>
2015-07-06 23:13:11 -0400

    adds development env

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-   connection: 'localDiskDb'
-  },
-  port: 2730
+    connection: 'stagingMongoDb'
+  }
+
 };
